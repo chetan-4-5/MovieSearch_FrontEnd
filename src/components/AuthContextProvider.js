87bd7f117416b9ac79473b821269bac03701dc07
Chetan Reddy <sharmila.chetan.2003@gmail.com>
2024-06-13 15:33:40 +0530

    update 2

--- src/components/AuthContextProvider.js
+++ src/components/AuthContextProvider.js
@@ -1,37 +1,33 @@
 // AuthContextProvider.js
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null); // Initialize with null or default state
+    const [currentUser, setCurrentUser] = useState(null);
 
     useEffect(() => {
-        // Function to fetch current user data from backend based on token
         const fetchCurrentUser = async () => {
-            const token = localStorage.getItem('token') || sessionStorage.getItem('token'); // Assuming token is stored in localStorage
+            const token = localStorage.getItem('token') || sessionStorage.getItem('token');
             if (token) {
                 try {
                     const response = await axios.get('https://moviesearch-backend-b97z.onrender.com/api/user', {
-                        headers: {
-                            'x-auth-token': localStorage.getItem('token') || sessionStorage.getItem('token')
-                        }
+                        headers: { 'x-auth-token': token }
                     });
-                    setCurrentUser(response.data.user); // Assuming response returns user data
+                    setCurrentUser(response.data);
                 } catch (error) {
                     console.error('Error fetching current user:', error);
-                    setCurrentUser(null); // Reset currentUser if there's an error
+                    setCurrentUser(null);
                 }
             } else {
-                setCurrentUser(null); // No token found, set currentUser to null
+                setCurrentUser(null);
             }
         };
 
-        fetchCurrentUser(); // Fetch current user on component mount
+        fetchCurrentUser();
     }, []);
-
-    // Functions to handle login, logout, etc. could be defined here
 
     return (
         <AuthContext.Provider value={{ currentUser }}>
